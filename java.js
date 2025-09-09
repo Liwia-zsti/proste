6eea0af51e5765fe7a3d9c0d2fbc1d78a8980314
Liwia-zsti <doszti123@gmail.com>
2025-09-09 09:55:58 +0200

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,13 @@
+let display = document.getElementById("wynik")
+let wojewodztwo = document.getElementById("t")
+let miasto = document.getElementById("miast")
+let atra = document.getElementById("atrakcja")
+let opis = document.getElementById("opo")
+let dodaj = document.getElementById("dodaj")
+let wyczysc = document.getElementById("wycz")
+
+
+
+
+document.getElementById("woj").innerHTML=(Option);
 
